0a786e7cb69f12fb414e646f7dcaae40248603de
Dorian Niemiec <dorian.niemiec@ferronweb.org>
2025-05-05 08:56:03 +0200

    chore: replace the repository with a notice that it has been moved

--- lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  "src/**/*.js": ["eslint --cache --fix", "prettier --write"],
-  "src/**/*.jsx": ["eslint --cache --fix", "prettier --write"],
-  "src/**/*.astro": ["eslint --cache --fix", "prettier --write"]
-};
